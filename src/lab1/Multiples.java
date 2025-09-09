daeaaad171219ac95a6692d46aa60b06e1bd0eaa
Lapis-Lazuli-31415 <andrewshen2020@gmail.com>
2025-09-08 23:18:23 -0400

    done multiples.java

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,16 +2,20 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
-        int num = 0;
-        for (int i = 1; i <= 1000; i++)
-        {
-            boolean divisible_by_3 = i % 3 == 0;
-            boolean divisible_by_5 = i % 5 == 0;
+        System.out.println(multiples(1000, 3, 5));
+    }
 
-            if (divisible_by_3 || divisible_by_5) {
-                num++;
+    public static int multiples(int n, int a, int b) {
+        int count = 0;
+        for (int i = 1; i < n; i++)
+        {
+            boolean divisible_by_a = i % a == 0;
+            boolean divisible_by_b = i % b == 0;
+
+            if (divisible_by_a || divisible_by_b) {
+                count++;
             }
         }
-        System.out.println(num);
+        return count;
     }
 }
