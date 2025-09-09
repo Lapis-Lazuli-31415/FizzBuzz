ad60cdce291a3a03e92d6a71e47ebe68cbf4502b
Lapis-Lazuli-31415 <andrewshen2020@gmail.com>
2025-09-08 23:42:58 -0400

    Finish Lab 1

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -5,6 +5,20 @@ public class Multiples {
         System.out.println(multiples(1000, 3, 5));
     }
 
+    public static int multiples() {
+        int count = 0;
+        for (int i = 1; i < 1000; i++)
+        {
+            boolean divisible_by_3 = i % 3 == 0;
+            boolean divisible_by_5 = i % 5 == 0;
+
+            if (divisible_by_3 || divisible_by_5) {
+                count++;
+            }
+        }
+        return count;
+    }
+
     public static int multiples(int n, int a, int b) {
         int count = 0;
         for (int i = 1; i < n; i++)
